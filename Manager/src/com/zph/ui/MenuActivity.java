15a82af15d27b7c60a541065cb327c6666750632
adminZPH <1364778591@qq.com>
2017-06-02 11:48:26 +0800

    2017-06-02 11:48

--- Manager/src/com/zph/ui/MenuActivity.java
+++ Manager/src/com/zph/ui/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.zph.ui;
 
 import java.awt.AWTException;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -21,11 +22,14 @@ import java.util.List;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
 
 import com.zph.pojo.User;
 import com.zph.service.UserService;
@@ -34,6 +38,7 @@ import com.zph.util.FileUtil;
 import com.zph.util.ListUtils;
 
 public class MenuActivity extends JFrame implements ActionListener {
+	private Container con;
 	// 设置全屏显示
 	private Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 	private JMenu jm = new JMenu("工作台1");
@@ -45,12 +50,17 @@ public class MenuActivity extends JFrame implements ActionListener {
 	// 最小化到系统托盘
 	private TrayIcon trayIcon;//  托盘图标  
 	private SystemTray systemTray;//  系统托盘
-	private Container con;
+
+	//列表
+	private JTable jLabel;
+	private String[] headTitle={"姓名","电话号码","地址","补充说明","录入时间"};
+	
 	
 	
 	private UserService userService;
 	public MenuActivity(String title) {
-
+		con=this.getContentPane();
+		
 		t1.addActionListener(this);
 		t2.addActionListener(this);
 		jm.add(t1); // 将菜单项目添加到菜单
@@ -61,7 +71,7 @@ public class MenuActivity extends JFrame implements ActionListener {
 		this.setJMenuBar(br); // 为 窗体设置 菜单工具栏
 		userService=new UserServiceImp();
 		initDate();
-		initSystemTray();
+//		initSystemTray();
 		this.setSize(d);
 		this.setTitle(title);
 		this.setVisible(true);
@@ -153,7 +163,9 @@ public class MenuActivity extends JFrame implements ActionListener {
 			new AddUserActivity();
 		} else if (e.getSource() == t2) {
 			List<User> list=userService.SelectAllUser();
-			System.out.println("最终的结果是："+list.toString());
+			//展示数据
+			showMyUser(list);
+			
 		}
 
 	}
@@ -177,5 +189,41 @@ public class MenuActivity extends JFrame implements ActionListener {
 			}
 		}
 	}
+	
+	/**
+	 * 以列表的形式展现出我的用户
+	 * */
+	private void showMyUser(List<User> list){
+		if(list.size()==0)
+			return;
+		Object[][] data =new Object[list.size()][5];
+		for(int i=0;i<list.size();++i){
+			System.out.println("姓名："+list.get(i).getUsername());
+			System.out.println("手机号："+list.get(i).getPhone());
+			System.out.println("address："+list.get(i).getAddress());
+			System.out.println("其它："+list.get(i).getOther());
+			System.out.println("时间："+list.get(i).getTime());
+		}
+		for(int i=0;i<list.size();++i){
+			data[i][0]=list.get(i).getUsername();
+			data[i][1]=list.get(i).getPhone();
+			data[i][2]=list.get(i).getAddress();
+			data[i][3]=list.get(i).getOther();
+			data[i][4]=list.get(i).getTime();
+		}
+//		jLabel=new JTable(data,headTitle);
+		DefaultTableModel model = new DefaultTableModel(data, headTitle) {
+			public boolean isCellEditable(int row, int column) {
+			    return false;
+			  }
+			};
+			jLabel = new JTable(model);
+		jLabel.setBounds(100, 100, d.width-100, d.height-100);
+		jLabel.setBackground(Color.WHITE);
+		
+		con.add(jLabel);
+	}
+	
+	
 
 }
